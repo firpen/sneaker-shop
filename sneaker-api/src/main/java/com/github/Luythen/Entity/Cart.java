a8a55a25cac98f10c5fc7bcf75ae2b9b6ff4d350
ninos11 <ninos.nina@hotmail.com>
2026-02-27 10:11:51 +0100

    cart entity

---
+++ sneaker-api/src/main/java/com/github/Luythen/Entity/Cart.java
@@ -0,0 +1,49 @@
+package com.github.Luythen.Entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "CART")
+public class Cart {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cartId")
+    private int cartId;
+    
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+    
+    @Column(name = "createdAt", nullable = false)
+    private LocalDateTime createdAt;
+    
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+    
+    public int getCartId() {
+        return cartId;
+    }
+    
+    public void setCartId(int cartId) {
+        this.cartId = cartId;
+    }
+    
+    public User getUser() {
+        return user;
+    }
+    
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
