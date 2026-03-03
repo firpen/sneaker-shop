e4c9530892cf047fcf8d126b4b6b40dfd6f1b539
DilerCiftci <diler-ciftci-94@hotmail.com>
2026-03-03 10:13:47 +0100

    Injected Product Service

--- sneaker-api/src/main/java/com/github/Luythen/Resource/ProductResource.java
+++ sneaker-api/src/main/java/com/github/Luythen/Resource/ProductResource.java
@@ -1,5 +1,18 @@
 package com.github.Luythen.Resource;
 
+import com.github.Luythen.Service.ProductService;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/api/products")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class ProductResource {
 
+    @Inject
+    ProductService productService;
 }
