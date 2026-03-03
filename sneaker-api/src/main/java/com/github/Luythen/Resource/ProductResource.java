b74a336049b451651fac202d4bf9a42a974ed0da
DilerCiftci <diler-ciftci-94@hotmail.com>
2026-03-03 10:33:30 +0100

    added create product

--- sneaker-api/src/main/java/com/github/Luythen/Resource/ProductResource.java
+++ sneaker-api/src/main/java/com/github/Luythen/Resource/ProductResource.java
@@ -1,12 +1,17 @@
 package com.github.Luythen.Resource;
 
+import com.github.Luythen.Entity.Product;
 import com.github.Luythen.Service.ProductService;
 
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -23,5 +28,26 @@ public class ProductResource {
     @GET
     public Response getAll() {
         return Response.ok(productService.getAllProducts()).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getById(@PathParam("id") Long id) {
+        Product product = productService.getProductById(id);
+        if (product == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.ok(product).build();
+    }
+
+    @POST
+    @RolesAllowed("Admin")
+    public Response create(Product product) {
+        try {
+            productService.createProduct(product);
+            return Response.status(Response.Status.CREATED).entity(product).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
     }
 }
