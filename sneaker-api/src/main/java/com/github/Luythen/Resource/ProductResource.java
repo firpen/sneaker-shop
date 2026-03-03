08598f605a0046f04b66fb347d4afd23498f080e
DilerCiftci <diler-ciftci-94@hotmail.com>
2026-03-03 10:18:56 +0100

    added GET products

--- sneaker-api/src/main/java/com/github/Luythen/Resource/ProductResource.java
+++ sneaker-api/src/main/java/com/github/Luythen/Resource/ProductResource.java
@@ -2,17 +2,26 @@ package com.github.Luythen.Resource;
 
 import com.github.Luythen.Service.ProductService;
 
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/api/products")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@RequestScoped
 public class ProductResource {
 
     @Inject
     ProductService productService;
+
+    @GET
+    public Response getAll() {
+        return Response.ok(productService.getAllProducts()).build();
+    }
 }
